f012fd3cc56385232b655cc899a4b05b208c14a3
leo <leonardo.groome@gmail.com>
2026-04-02 20:55:08 -0600

    feat: integrate PostHog analytics provider into the application root

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,20 @@ import './i18n'
 import App from './App.tsx'
 
 import { BrowserRouter } from 'react-router-dom'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+posthog.init('phc_sdZv8T9Q2Bv6CsktmA5XELsWkwvKbhnyeHF75zwcBfKe', {
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'identified_only',
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PostHogProvider client={posthog}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PostHogProvider>
   </StrictMode>,
 )
